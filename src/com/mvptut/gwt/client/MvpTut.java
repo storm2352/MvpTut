d7a2ace36486a73292e66bcf6d4478597c7c9a36
luke <storm2352@gmail.com>
2013-09-19 15:20:45 +0200

    Person MVP

--- src/com/mvptut/gwt/client/MvpTut.java
+++ src/com/mvptut/gwt/client/MvpTut.java
@@ -2,15 +2,23 @@ package com.mvptut.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.mvptut.gwt.shared.Person;
 
 public class MvpTut implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
 		
-		Button btn = new Button("Hello World");
-		RootPanel.get().add(btn);
+		Person me = new Person();
+		Label lMe = new Label("Hello my name is " + me.fullName());
+		
+		Person other = new Person("Peter", "Pumkin");
+		Label lOther = new Label("The other name is " + other.fullName());
+		
+		RootPanel.get().add(lMe);
+		RootPanel.get().add(lOther);
 
 	}
 
