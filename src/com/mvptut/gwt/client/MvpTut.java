5bd2ea55b18ed2f318e80b9d24879841e8062676
luke <storm2352@gmail.com>
2013-09-19 16:44:06 +0200

    MVP with View

--- src/com/mvptut/gwt/client/MvpTut.java
+++ src/com/mvptut/gwt/client/MvpTut.java
@@ -1,9 +1,11 @@
 package com.mvptut.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.mvptut.gwt.client.presenters.PersonPresenter;
+import com.mvptut.gwt.client.presenters.PersonPresenter.Display;
+import com.mvptut.gwt.client.presenters.Presenter;
+import com.mvptut.gwt.client.views.PersonView;
 import com.mvptut.gwt.shared.Person;
 
 public class MvpTut implements EntryPoint {
@@ -12,13 +14,9 @@ public class MvpTut implements EntryPoint {
 	public void onModuleLoad() {
 		
 		Person me = new Person();
-		Label lMe = new Label("Hello my name is " + me.fullName());
-		
-		Person other = new Person("Peter", "Pumkin");
-		Label lOther = new Label("The other name is " + other.fullName());
-		
-		RootPanel.get().add(lMe);
-		RootPanel.get().add(lOther);
+		Display view = new PersonView() ;
+		Presenter presenter = new PersonPresenter(me, view);
+		presenter.go(RootPanel.get());
 
 	}
 
